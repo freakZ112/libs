f8f225bd25664caeadfb0882a60708e97de57715
freakZ112 <102925064+freakZ112@users.noreply.github.com>
2023-05-27 23:53:35 +0200

    Create buildings.js

---
+++ buildings.js
@@ -0,0 +1,783 @@
+[
+	{
+		"id": 0,
+		"name": "Feuerwache",
+		"class": ["fire_stations"],
+		"class_alias": ["Feuerwachen"],
+		"max_level": 16,
+		"extensions": [
+			{
+				"id": 0,
+				"name": "Rettungsdienst-Erweiterung",
+				"cost": 1e5,
+				"duration": 7
+			},
+			{
+				"id": 1,
+				"name": "Abrollbehälter-Stellplatz",
+				"cost": 1e5,
+				"duration": 7
+			},
+			{
+				"id": 2,
+				"name": "Abrollbehälter-Stellplatz",
+				"cost": 1e5,
+				"duration": 7
+			},
+			{
+				"id": 3,
+				"name": "Abrollbehälter-Stellplatz",
+				"cost": 1e5,
+				"duration": 7
+			},
+			{
+				"id": 4,
+				"name": "Abrollbehälter-Stellplatz",
+				"cost": 1e5,
+				"duration": 7
+			},
+			{
+				"id": 5,
+				"name": "Abrollbehälter-Stellplatz",
+				"cost": 1e5,
+				"duration": 7
+			},
+			{
+				"id": 6,
+				"name": "Wasserrettungs-Erweiterung",
+				"cost": 4e5,
+				"duration": 7
+			},
+			{
+				"id": 7,
+				"name": "Abrollbehälter-Stellplatz",
+				"cost": 1e5,
+				"duration": 7
+			},
+			{
+				"id": 8,
+				"name": "Flughafen-Erweiterung",
+				"cost": 3e5,
+				"duration": 7
+			},
+			{
+				"id": 9,
+				"name": "Großwache",
+				"cost": 1e6,
+				"duration": 7
+			},
+			{
+				"id": 10,
+				"name": "Abrollbehälter-Stellplatz",
+				"cost": 1e5,
+				"duration": 7
+			},
+			{
+				"id": 11,
+				"name": "Abrollbehälter-Stellplatz",
+				"cost": 1e5,
+				"duration": 7
+			},
+			{
+				"id": 12,
+				"name": "Abrollbehälter-Stellplatz",
+				"cost": 1e5,
+				"duration": 7
+			},
+			{
+				"id": 13,
+				"name": "Werkfeuerwehr",
+				"cost": 1e5,
+				"duration": 7
+			}
+		]
+	},
+	{
+		"id": 1,
+		"name": "Feuerwehrschule",
+		"class": [],
+		"class_alias": [],
+		"max_level": 3,
+		"extensions": [
+			{
+				"id": 0,
+				"name": "Weiterer Klassenraum",
+				"cost": 4e5,
+				"duration": 7
+			},
+			{
+				"id": 1,
+				"name": "Weiterer Klassenraum",
+				"cost": 4e5,
+				"duration": 7
+			},
+			{
+				"id": 2,
+				"name": "Weiterer Klassenraum",
+				"cost": 4e5,
+				"duration": 7
+			}
+		]
+	},
+	{
+		"id": 2,
+		"name": "Rettungswache",
+		"class": ["rescue_stations"],
+		"class_alias": ["Rettungswachen"],
+		"max_level": 14,
+		"extensions": []
+	},
+	{
+		"id": 3,
+		"name": "Rettungsdienstschule",
+		"class": [],
+		"class_alias": [],
+		"max_level": 3,
+		"extensions": [
+			{
+				"id": 0,
+				"name": "Weiterer Klassenraum",
+				"cost": 4e5,
+				"duration": 7
+			},
+			{
+				"id": 1,
+				"name": "Weiterer Klassenraum",
+				"cost": 4e5,
+				"duration": 7
+			},
+			{
+				"id": 2,
+				"name": "Weiterer Klassenraum",
+				"cost": 4e5,
+				"duration": 7
+			}
+		]
+	},
+	{
+		"id": 4,
+		"name": "Krankenhaus",
+		"class": [],
+		"class_alias": [],
+		"max_level": 20,
+		"extensions": [
+			{
+				"id": 0,
+				"name": "Allgemeine Innere",
+				"cost": 1e4,
+				"duration": 7
+			},
+			{
+				"id": 1,
+				"name": "Allgemeine Chirurgie",
+				"cost": 1e4,
+				"duration": 7
+			},
+			{
+				"id": 2,
+				"name": "Gynäkologie",
+				"cost": 7e4,
+				"duration": 7
+			},
+			{
+				"id": 3,
+				"name": "Urologie",
+				"cost": 7e4,
+				"duration": 7
+			},
+			{
+				"id": 4,
+				"name": "Unfallchirurgie",
+				"cost": 7e4,
+				"duration": 7
+			},
+			{
+				"id": 5,
+				"name": "Neurologie",
+				"cost": 7e4,
+				"duration": 7
+			},
+			{
+				"id": 6,
+				"name": "Neurochirurgie",
+				"cost": 7e4,
+				"duration": 7
+			},
+			{
+				"id": 7,
+				"name": "Kardiologie",
+				"cost": 7e4,
+				"duration": 7
+			},
+			{
+				"id": 8,
+				"name": "Kardiochirurgie",
+				"cost": 7e4,
+				"duration": 7
+			}
+		]
+	},
+	{
+		"id": 5,
+		"name": "Rettungshubschrauber-Station",
+		"class": [],
+		"class_alias": [],
+		"max_level": null,
+		"extensions": []
+	},
+	{
+		"id": 6,
+		"name": "Polizeiwache",
+		"class": ["police_stations"],
+		"class_alias": ["Polizeiwachen"],
+		"max_level": 14,
+		"extensions": [
+			{
+				"id": 0,
+				"name": "Zelle",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 1,
+				"name": "Zelle",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 2,
+				"name": "Zelle",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 3,
+				"name": "Zelle",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 4,
+				"name": "Zelle",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 5,
+				"name": "Zelle",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 6,
+				"name": "Zelle",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 7,
+				"name": "Zelle",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 8,
+				"name": "Zelle",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 9,
+				"name": "Zelle",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 10,
+				"name": "Diensthundestaffel",
+				"cost": 1e5,
+				"duration": 5
+			},
+			{
+				"id": 11,
+				"name": "Kriminalpolizei-Erweiterung",
+				"cost": 1e5,
+				"duration": 7
+			},
+			{
+				"id": 12,
+				"name": "Dienstgruppenleitung-Erweiterung",
+				"cost": 2e5,
+				"duration": 7
+			},
+			{
+				"id": 13,
+				"name": "Motorradstaffel",
+				"cost": 75e3,
+				"duration": 2
+			}
+		]
+	},
+	{
+		"id": 7,
+		"name": "Leitstelle",
+		"class": [],
+		"class_alias": [],
+		"max_level": 0,
+		"extensions": []
+	},
+	{
+		"id": 8,
+		"name": "Polizeischule",
+		"class": [],
+		"class_alias": [],
+		"max_level": 3,
+		"extensions": [
+			{
+				"id": 0,
+				"name": "Weiterer Klassenraum",
+				"cost": 4e5,
+				"duration": 7
+			},
+			{
+				"id": 1,
+				"name": "Weiterer Klassenraum",
+				"cost": 4e5,
+				"duration": 7
+			},
+			{
+				"id": 2,
+				"name": "Weiterer Klassenraum",
+				"cost": 4e5,
+				"duration": 7
+			}
+		]
+	},
+	{
+		"id": 9,
+		"name": "THW",
+		"class": ["thw"],
+		"class_alias": ["THW-Ortsverbände"],
+		"max_level": 0,
+		"extensions": [
+			{
+				"id": 0,
+				"name": "1. Technischer Zug: Fachgruppe Notversorgung/Notinstandsetzung",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 1,
+				"name": "1. Technischer Zug: Zugtrupp",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 2,
+				"name": "Fachgruppe Räumen",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 3,
+				"name": "Fachgruppe Wassergefahren",
+				"cost": 5e5,
+				"duration": 7
+			},
+			{
+				"id": 4,
+				"name": "2. Technischer Zug - Grundvorraussetzungen",
+				"cost": 25e3,
+				"duration": 3
+			},
+			{
+				"id": 5,
+				"name": "2. Technischer Zug: Fachgruppe Notversorgung/Notinstandsetzung",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 6,
+				"name": "2. Technischer Zug: Zugtrupp",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 7,
+				"name": "Fachgruppe Ortung",
+				"cost": 45e4,
+				"duration": 7
+			},
+			{
+				"id": 8,
+				"name": "Fachgruppe Wasserschaden/Pumpen",
+				"cost": 2e5,
+				"duration": 7
+			},
+			{
+				"id": 9,
+				"name": "Fachgruppe Schwere Bergung",
+				"cost": 2e5,
+				"duration": 7
+			},			
+			{
+				"id": 10,
+				"name": "Fachgruppe Elektroversorgung",
+				"cost": 2e5,
+				"duration": 7
+			}				
+		]
+	},
+	{
+		"id": 10,
+		"name": "THW-Bundesschule",
+		"class": [],
+		"class_alias": [],
+		"max_level": 3,
+		"extensions": [
+			{
+				"id": 0,
+				"name": "Weiterer Klassenraum",
+				"cost": 4e5,
+				"duration": 7
+			},
+			{
+				"id": 1,
+				"name": "Weiterer Klassenraum",
+				"cost": 4e5,
+				"duration": 7
+			},
+			{
+				"id": 2,
+				"name": "Weiterer Klassenraum",
+				"cost": 4e5,
+				"duration": 7
+			}
+		]
+	},
+	{
+		"id": 11,
+		"name": "Bereitschaftspolizei",
+		"class": ["bereitschaftspolizei", "sek", "mek"],
+		"class_alias": ["Bereitschaftspolizei", "SEK", "MEK"],
+		"max_level": 0,
+		"extensions": [
+			{
+				"id": 0,
+				"name": "2. Zug der 1. Hundertschaft",
+				"cost": 25e3,
+				"duration": 3
+			},
+			{
+				"id": 1,
+				"name": "3. Zug der 1. Hundertschaft",
+				"cost": 25e3,
+				"duration": 3
+			},
+			{
+				"id": 2,
+				"name": "Sonderfahrzeug: Gefangenenkraftwagen",
+				"cost": 25e3,
+				"duration": 3
+			},
+			{
+				"id": 3,
+				"name": "Technischer Zug: Wasserwerfer",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 4,
+				"name": "SEK: 1. Zug",
+				"cost": 1e5,
+				"duration": 7
+			},
+			{
+				"id": 5,
+				"name": "SEK: 2. Zug",
+				"cost": 1e5,
+				"duration": 7
+			},
+			{
+				"id": 6,
+				"name": "MEK: 1. Zug",
+				"cost": 1e5,
+				"duration": 7
+			},
+			{
+				"id": 7,
+				"name": "MEK: 2. Zug",
+				"cost": 1e5,
+				"duration": 7
+			},
+			{
+				"id": 8,
+				"name": "Diensthundestaffel",
+				"cost": 1e5,
+				"duration": 5
+			}
+		]
+	},
+	{
+		"id": 12,
+		"name": "Schnelleinsatzgruppe (SEG)",
+		"class": ["seg"],
+		"class_alias": ["Schnelleinsatzgruppen (SEG)"],
+		"max_level": 0,
+		"extensions": [
+			{
+				"id": 0,
+				"name": "Führung",
+				"cost": 25e3,
+				"duration": 3
+			},
+			{
+				"id": 1,
+				"name": "Sanitätsdienst",
+				"cost": 25e3,
+				"duration": 3
+			},
+			{
+				"id": 2,
+				"name": "Wasserrettungs-Erweiterung",
+				"cost": 5e5,
+				"duration": 7
+			},
+			{
+				"id": 3,
+				"name": "Rettungshundestaffel",
+				"cost": 35e4,
+				"duration": 7
+			}
+		]
+	},
+	{
+		"id": 13,
+		"name": "Polizeihubschrauber-Station",
+		"class": ["police_helicopter_stations"],
+		"class_alias": ["Polizeihubschrauber-Stationen"],
+		"max_level": null,
+		"extensions": [
+			{
+				"id": 0,
+				"name": " Außenlastbehälter-Erweiterung",
+				"cost": 2e5,
+				"duration": 3
+			}
+		]
+	},
+	{
+		"id": 14,
+		"name": "Bereitstellungsraum",
+		"class": [],
+		"class_alias": [],
+		"max_level": 0,
+		"extensions": []
+	},
+	{
+		"id": 15,
+		"name": "Wasserrettung",
+		"class": ["wasserwacht"],
+		"class_alias": ["Wasserrettungswachen"],
+		"max_level": 5,
+		"extensions": []
+	},
+	{
+		"id": 16,
+		"name": "Verbandszellen",
+		"class": [],
+		"class_alias": [],
+		"max_level": 0,
+		"extensions": [
+			{
+				"id": 0,
+				"name": "Zelle",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 1,
+				"name": "Zelle",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 2,
+				"name": "Zelle",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 3,
+				"name": "Zelle",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 4,
+				"name": "Zelle",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 5,
+				"name": "Zelle",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 6,
+				"name": "Zelle",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 7,
+				"name": "Zelle",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 8,
+				"name": "Zelle",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 9,
+				"name": "Zelle",
+				"cost": 25e3,
+				"duration": 7
+			}
+		]
+	},
+	{
+		"id": 17,
+		"name": "Polizei-Sondereinheiten",
+		"class": ["sek", "mek"],
+		"class_alias": ["SEK", "MEK"],
+		"max_level": 0,
+		"extensions": [
+			{
+				"id": 0,
+				"name": "SEK: 1. Zug",
+				"cost": 1e5,
+				"duration": 7
+			},
+			{
+				"id": 1,
+				"name": "SEK: 2. Zug",
+				"cost": 1e5,
+				"duration": 7
+			},
+			{
+				"id": 2,
+				"name": "MEK: 1. Zug",
+				"cost": 1e5,
+				"duration": 7
+			},
+			{
+				"id": 3,
+				"name": "MEK: 2. Zug",
+				"cost": 1e5,
+				"duration": 7
+			},
+			{
+				"id": 4,
+				"name": "Diensthundestaffel",
+				"cost": 1e5,
+				"duration": 5
+			}
+		]
+	},
+	{
+		"id": 18,
+		"name": "Feuerwache (klein)",
+		"class": ["fire_stations"],
+		"class_alias": ["Feuerwachen"],
+		"max_level": 5,
+		"extensions": [
+			{
+				"id": 1,
+				"name": "Abrollbehälter-Stellplatz",
+				"cost": 1e5,
+				"duration": 7
+			},
+			{
+				"id": 2,
+				"name": "Abrollbehälter-Stellplatz",
+				"cost": 1e5,
+				"duration": 7
+			}
+		]
+	},
+	{
+		"id": 19,
+		"name": "Polizeiwache (klein)",
+		"class": ["police_stations"],
+		"class_alias": ["Polizeiwachen"],
+		"max_level": 5,
+		"extensions": [
+			{
+				"id": 0,
+				"name": "Zelle",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 1,
+				"name": "Zelle",
+				"cost": 25e3,
+				"duration": 7
+			},
+			{
+				"id": 10,
+				"name": "Diensthundestaffel",
+				"cost": 1e5,
+				"duration": 5
+			},
+			{
+				"id": 11,
+				"name": "Kriminalpolizei-Erweiterung",
+				"cost": 1e5,
+				"duration": 7
+			},
+			{
+				"id": 12,
+				"name": "Dienstgruppenleitung-Erweiterung",
+				"cost": 2e5,
+				"duration": 7
+			},
+			{
+				"id": 13,
+				"name": "Motorradstaffel",
+				"cost": 75e3,
+				"duration": 2
+			}
+		]
+	},
+	{
+		"id": 20,
+		"name": "Rettungswache (klein)",
+		"class": ["rescue_stations"],
+		"class_alias": ["Rettungswachen"],
+		"max_level": 5,
+		"extensions": []
+	},
+	{
+		"id": 21,
+		"name": "Rettungshundestaffel",
+		"class": ["rescue_dog_units"],
+		"class_alias": ["Rettungshundestaffeln"],
+		"max_level": 6,
+		"extensions": []
+	},
+	{
+		"id": 22,
+		"name": "Gebäudekomplex",
+		"class": [],
+		"class_alias": [],
+		"max_level": 20,
+		"extensions": []
+	}
+]
